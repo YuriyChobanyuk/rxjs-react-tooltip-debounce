485deeb36ff6aedb9e58f5a4567c8f482825b0d5
Yurii Chobaniuk <sonicmaster172@gmail.com>
2020-07-10 19:39:24 +0300

    v 0.1.1

--- src/useDebouncedTooltip.ts
+++ src/useDebouncedTooltip.ts
@@ -1,89 +1,55 @@
-import { map, filter, debounceTime, mergeMap } from "rxjs/operators";
+import { map, filter, debounceTime, takeUntil, tap } from "rxjs/operators";
 import { RefObject, useState, useEffect } from "react";
-import { fromEvent, BehaviorSubject } from "rxjs";
+import { fromEvent, Subject } from "rxjs";
 
 export const useDebouncedTooltip = (
   containerRef: RefObject<HTMLElement>,
   elementClass: string
 ) => {
   const [currentElementId, setCurrentElementId] = useState("");
-  const [availableTooltipSubject$] = useState(new BehaviorSubject(true));
 
   useEffect(() => {
     if (containerRef === null || containerRef.current === null) return;
+    let availableTooltip = false;
+    const destroy$ = new Subject();
 
-    const mouseOverSubscription = fromEvent(containerRef.current, "mouseover")
+    fromEvent(containerRef.current, "mouseover")
       .pipe(
-        mergeMap((event) =>
-          availableTooltipSubject$.pipe(
-            map((availableTooltip) => ({
-              event,
-              availableTooltip,
-            }))
-          )
-        ),
+        tap(() => {
+          availableTooltip = true;
+        }),
         debounceTime(1000),
-        filter(({ event, availableTooltip }) => {
+        filter((event) => {
           const target = event.target as EventTarget & HTMLSpanElement;
           return target?.classList?.contains(elementClass) && availableTooltip;
         }),
-        map(({ event }) => {
+        map((event) => {
           const target = event.target as EventTarget & HTMLSpanElement;
           return target.id;
-        })
+        }),
+        takeUntil(destroy$)
       )
       .subscribe((id) => {
         setCurrentElementId(id);
       });
 
-    const mouseOutSubscription = fromEvent(containerRef?.current, "mouseout")
+    fromEvent(containerRef?.current, "mouseout")
       .pipe(
         filter((event) => {
           const target = event.target as EventTarget & HTMLSpanElement;
           return target?.classList?.contains(elementClass);
-        })
+        }),
+        takeUntil(destroy$)
       )
       .subscribe(() => {
+        availableTooltip = false;
         setCurrentElementId("");
       });
 
     return () => {
-      mouseOutSubscription.unsubscribe();
-      mouseOverSubscription.unsubscribe();
+      destroy$.next(true);
     };
-  }, [containerRef, availableTooltipSubject$, elementClass]);
-
-  useEffect(() => {
-    if (containerRef === null || containerRef.current === null) return;
-
-    const mouseLeaveSubscription = fromEvent(
-      containerRef?.current,
-      "mouseleave"
-    )
-      .pipe()
-      .subscribe((id) => {
-        setCurrentElementId("");
-        availableTooltipSubject$.next(false);
-      });
-
-    const mouseEnterSubscription = fromEvent(
-      containerRef?.current,
-      "mouseenter"
-    ).subscribe(() => {
-      availableTooltipSubject$.next(true);
-    });
-
-    return () => {
-      mouseLeaveSubscription.unsubscribe();
-      mouseEnterSubscription.unsubscribe();
-    };
-  }, [containerRef, availableTooltipSubject$]);
-
-  useEffect(() => {
-    return () => {
-      availableTooltipSubject$.unsubscribe();
-    };
-  }, [availableTooltipSubject$]);
+  }, [containerRef, elementClass]);
 
   return currentElementId;
 };
